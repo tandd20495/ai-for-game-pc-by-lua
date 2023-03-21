88af4526440b5ce18de886c95e693a6876af43dc
TanDD <tandd20495@gmail.com>
2023-03-21 19:12:42 +0700

    Add spam skill function

--- yBreaker/Source/admin_yBreaker/lua/admin_yBreaker/yBreaker_admin_libraries/yBreaker_libs.lua
+++ yBreaker/Source/admin_yBreaker/lua/admin_yBreaker/yBreaker_admin_libraries/yBreaker_libs.lua
@@ -16,7 +16,7 @@ function yBreaker_show_notice(info, noticetype)
 end
 
 -- Function to write log
-function console(str, isdebug)
+function yBreaker_console(str, isdebug)
 	local file = io.open("D:\\log_yBreaker.txt", "a")
 	if file == nil then
 		nx_value("SystemCenterInfo"):ShowSystemCenterInfo(nx_widestr("Can't open file D:\\log_yBreaker.txt, please check this file!"), 3)
@@ -30,4 +30,26 @@ function console(str, isdebug)
 		file:write("\n")
 		file:close()
 	end
+end
+
+-- Function get buff exist on player
+function yBreaker_get_id_buff_info(buff_id)
+	-- Nếu tồn tại buff_id thì trả về thời gian của buff đó, nếu buff không có thời gian thì trả về -1
+	-- Nếu không tồn tại buff thì thả về nil
+	local form = nx_value("form_stage_main\\form_main\\form_main_buff")
+	if not nx_is_valid(form) then
+		return nil
+	end
+	
+	for i = 1, 24 do
+		if nx_string(form["lbl_photo" .. tostring(i)].buffer_id) == buff_id then
+			local buff_time = get_second_from_text(form["lbl_time" .. tostring(i)].Text)
+			if buff_time == nil then
+				return -1
+			else
+				return buff_time
+			end
+		end
+	end
+	return nil
 end
---
+++ yBreaker/Source/admin_yBreaker/lua/admin_yBreaker/yBreaker_scripts_func/yBreaker_scripts_spamskill.lua
@@ -0,0 +1,40 @@
+require("const_define")
+require("util_gui")
+require("util_functions")
+require("share\\server_custom_define")
+require("define\\sysinfo_define")
+require("share\\chat_define")
+require("define\\request_type")
+require("admin_yBreaker\\yBreaker_admin_libraries\\yBreaker_libs")
+
+
+ local isRunning=false
+ 
+ function spam_Skill()
+ local FORM_SHORTCUT = "form_stage_main\\form_main\\form_main_shortcut"
+ local game_shortcut = nx_value("GameShortcut")
+ local form = nx_value(FORM_SHORTCUT)
+ local grid = form.grid_shortcut_main
+ 	if not isRunning then
+		isRunning = true
+		
+		yBreaker_show_notice(util_text("Start"))
+			while isRunning == true do
+				local isBuff = yBreaker_get_id_buff_info("buf_CS_jl_shuangci07")
+				if (isBuff == nil or isBuff < 5) then
+				-- Buff ôn thần còn 5s thì nộ ( phím số 5 )
+					game_shortcut:on_main_shortcut_useitem(grid, 4, true)
+					nx_pause(0.1)
+				end
+				-- Nhấn 1234 liên tục
+					game_shortcut:on_main_shortcut_useitem(grid, 0, true)
+					game_shortcut:on_main_shortcut_useitem(grid, 1, true)
+					game_shortcut:on_main_shortcut_useitem(grid, 2, true)
+					game_shortcut:on_main_shortcut_useitem(grid, 3, true)
+					nx_pause(0.1)
+			end
+	else
+		isRunning = false
+		yBreaker_show_notice(util_text("Stop"))
+	end
+end
