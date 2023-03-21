7c93c3791f5de67405ad1a59a94a953f49b2e811
TanDD <tandd20495@gmail.com>
2023-03-21 16:39:00 +0700

    Delete code is not use in yBreaker_libs.lua Change structure debg test function Add grocery for yBreaker Add teleport for yBreaker

---
+++ yBreaker/Source/admin_yBreaker/lua/admin_yBreaker/yBreaker_scripts_func/yBreaker_scripts_grocery.lua
@@ -0,0 +1,13 @@
+-- Show/Hide for grocery of admin yBreaker
+function show_hide_grocery()
+
+	local form_shop = nx_value("form_stage_main\\form_shop\\form_shop")
+	
+    if nx_is_valid(form_shop) then
+		nx_execute("custom_sender", "custom_open_mount_shop", 0)
+    else
+		nx_execute("custom_sender", "custom_open_mount_shop", 1)
+    end
+end
+-- Exceute function
+show_hide_grocery()
