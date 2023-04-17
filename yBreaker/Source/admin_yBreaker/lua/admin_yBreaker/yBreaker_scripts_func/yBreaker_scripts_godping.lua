88473f0659d0bfe9f40557a4becef575ef29c53d
TanDD <tandd20495@gmail.com>
2023-04-17 16:18:04 +0700

    Get error when go LSV

--- yBreaker/Source/admin_yBreaker/lua/admin_yBreaker/yBreaker_scripts_func/yBreaker_scripts_godping.lua
+++ yBreaker/Source/admin_yBreaker/lua/admin_yBreaker/yBreaker_scripts_func/yBreaker_scripts_godping.lua
@@ -7,11 +7,34 @@ function yBreaker_god_ping()
 		yBreaker_show_Utf8Text("Đè ping chi thuật")
 		while isPing == true do
 			nx_pause(0)
+			local is_vaild_data = true
 			local game_client = nx_value("game_client")
+			if not nx_is_valid(game_client) then
+				is_vaild_data = false
+			end
+			
 			local client_scene_obj = game_client:GetPlayer()
+			-- Hứng lỗi khi vào liên server
+			if client_scene_obj == false then
+				isPing = false
+				is_vaild_data = false
+				yBreaker_show_Utf8Text("Dừng cấm thuật, khởi động lại khi vào liên server!")
+				return
+			end
+			
 			local game_visual = nx_value("game_visual")
+			if not nx_is_valid(game_visual) then
+				is_vaild_data = false
+			end
+			
 			local visual_scene_obj = game_visual:GetSceneObj(client_scene_obj.Ident)
-			game_visual:EmitPlayerInput(visual_scene_obj, 21, 12)
+			if not nx_is_valid(visual_scene_obj) then
+				is_vaild_data = false
+			end
+			
+			if is_vaild_data then
+				game_visual:EmitPlayerInput(visual_scene_obj, 21, 12)
+			end
 		end
 	else
 		isPing = false
