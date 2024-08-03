c2e0df74118a1fdbd6acf63466fa39ffaa201f75
TanDD <tandd20495@gmail.com>
2024-08-03 13:13:44 +0700

    Update debug test function

--- yBreaker/Source_x64/lua64/admin_yBreaker/yBreaker_scripts_func/yBreaker_scripts_debugtest.lua
+++ yBreaker/Source_x64/lua64/admin_yBreaker/yBreaker_scripts_func/yBreaker_scripts_debugtest.lua
@@ -3,5 +3,5 @@ require("util_gui")
 -- Auto show/hide form debug test ingame
 function show_hide_form_debug_test_yBreaker()
 	util_auto_show_hide_form("form_test\\form_debug")
-	--util_auto_show_hide_form("move_test\\form_collide_test")
+	util_auto_show_hide_form("form_test\\form_collide_test")
 end
